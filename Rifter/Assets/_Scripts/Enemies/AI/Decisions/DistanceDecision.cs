bece3f88f431255a8674b797f5285288a0cadd57
Timothy Rasmussen <Timothy.s.ramsussen@gmail.com>
2021-03-11 22:25:30 +0100

    Implementing AI Logic

---
+++ Rifter/Assets/_Scripts/Enemies/AI/Decisions/DistanceDecision.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts.Enemies.AI.Decisions
+{
+    public class DistanceDecision : AIDecision
+    {
+        [field: SerializeField] [field: Range(0.1f, 10)] public float Distance { get; set; } = 5;
+        
+        public override bool MakeADecision()
+        {
+            if (Vector3.Distance(EnemyController.Target.transform.position, transform.position) < Distance)
+            {
+                if (AIActionData.TargetSpotted == false)
+                {
+
+                    AIActionData.TargetSpotted = true;
+                }
+            }
+            else
+            {
+                AIActionData.TargetSpotted = false;
+            }
+            
+            return AIActionData.TargetSpotted;
+            
+        }
+
+        protected void OnDrawGizmos()
+        {
+            if (UnityEditor.Selection.activeObject == gameObject)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireSphere(transform.position, Distance);
+                Gizmos.color = Color.white;
+            }
+        }
+    }
+}
