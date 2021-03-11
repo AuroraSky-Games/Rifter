c803510753b444abe78785d9bf46851aab9ed98a
Timothy Rasmussen <Timothy.s.ramsussen@gmail.com>
2021-03-11 22:55:46 +0100

    Done for today

---
+++ Rifter/Assets/_Scripts/Enemies/AI/Actions/ChaseAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Scripts.Enemies.AI.Actions
+{
+    public class ChaseAction : AIAction
+    {
+        public override void TakeAction()
+        {
+            var position = EnemyController.Target.transform.position;
+            var direction = position - transform.position;
+            AIMovementData.Direction = direction.normalized;
+            AIMovementData.PointOfInterest = position;
+            EnemyController.Move(AIMovementData.Direction, AIMovementData.PointOfInterest);
+        }
+    }
+}
--- Rifter/Assets/_Scripts/Enemies/EnemyController.cs
+++ Rifter/Assets/_Scripts/Enemies/EnemyController.cs
@@ -1,49 +1,49 @@
 using System;
 using _Scripts.Enemies.AI;
+using _Scripts.Player;
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace _Scripts.Enemies
+
+public class EnemyController : MonoBehaviour
 {
-    public class EnemyController : MonoBehaviour
+    [field: SerializeField] public GameObject Target { get; set; }
+    [field: SerializeField] public AIState CurrentState { get; private set; }
+    [field: SerializeField] public UnityEvent<Vector2> OnTargetChange { get; set; }
+    [field: SerializeField] public UnityEvent<Vector2> OnRun { get; set; }
+    [field: SerializeField] public UnityEvent<Vector2> OnStop { get; set;}
+    [field: SerializeField] public UnityEvent<float> OnVelocityChange { get; set; }
+    [field: SerializeField] public UnityEvent OnAttack { get; set; }
+    [field: SerializeField] public UnityEvent OnStopAttack { get; set; }
+
+    private void Awake()
     {
-        [field: SerializeField] public GameObject Target { get; set; }
-        [field: SerializeField] public AIState CurrentState { get; private set; }
-        [field: SerializeField] public UnityEvent<Vector2> OnTargetChange { get; set; }
-        [field: SerializeField] public UnityEvent<Vector2> OnRun { get; set; }
-        [field: SerializeField] public UnityEvent<Vector2> OnStop { get; set;}
-        [field: SerializeField] public UnityEvent<float> OnVelocityChange { get; set; }
-        [field: SerializeField] public UnityEvent OnAttack { get; set; }
-        [field: SerializeField] public UnityEvent OnStopAttack { get; set; }
+        Target = FindObjectOfType<Player>().gameObject; 
+    }
 
-        private void Awake()
+    private void Update()
+    {
+        if (Target == null)
         {
-            Target = FindObjectOfType<Player.Player>().gameObject;
+            OnRun?.Invoke(Vector2.zero);
         }
+        CurrentState.UpdateState();
+    }
 
-        private void Update()
-        {
-            if (Target == null)
-            {
-                OnRun?.Invoke(Vector2.zero);
-            }
-            CurrentState.UpdateState();
-        }
+    public void Attack()
+    {
+        OnAttack?.Invoke();
+    }
 
-        public void Attack()
-        {
-            OnAttack?.Invoke();
-        }
+    public void Move(Vector2 movementDirection, Vector2 targetPosition)
+    {
+        OnRun?.Invoke(movementDirection);
+        OnTargetChange?.Invoke(targetPosition);
+    }
 
-        public void Move(Vector2 movementDirection, Vector2 targetPosition)
-        {
-            OnRun?.Invoke(movementDirection);
-            OnTargetChange?.Invoke(targetPosition);
-        }
-
-        public void ChangeToState(AIState state)
-        {
-            CurrentState = state;
-        }
+    public void ChangeToState(AIState state)
+    {
+        CurrentState = state;
     }
 }
+
