6655758ea66d4524ccacaa383bd6ca23350986a7
Timothy Rasmussen <Timothy.s.rasmussen@gmail.com>
2021-03-20 10:49:14 +0100

    Player Destroy Error Fixed

--- Rifter/Assets/_Scripts/Enemies/AI/Actions/ChaseAction.cs
+++ Rifter/Assets/_Scripts/Enemies/AI/Actions/ChaseAction.cs
@@ -7,13 +7,16 @@ namespace _Scripts.Enemies.AI.Actions
     {
         public override void TakeAction()
         {
-            AIActionData.Attack = false;
-            AIActionData.Arrived = false;
-            var position = EnemyController.Target.transform.position;
-            var direction = position - transform.position;
-            AIMovementData.Direction = direction.normalized;
-            AIMovementData.PointOfInterest = position;
-            EnemyController.Move(AIMovementData.Direction, AIMovementData.PointOfInterest);
+            if (EnemyController.Target != null)
+            {
+                AIActionData.Attack = false;
+                AIActionData.Arrived = false;
+                var position = EnemyController.Target.transform.position;
+                var direction = position - transform.position;
+                AIMovementData.Direction = direction.normalized;
+                AIMovementData.PointOfInterest = position;
+                EnemyController.Move(AIMovementData.Direction, AIMovementData.PointOfInterest);
+            }
         }
     }
 }
